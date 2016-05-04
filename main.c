d6c3ce68af8ba3504c9f5236a67bca9d38450085
Amar Duranovic <amard15@student.uia.no>
2016-05-04 14:50:14 +0200

    Text changes and main.c changes

--- main.c
+++ main.c
@@ -19,23 +19,7 @@ int main(int argc, char* argv[])
 
     while(CH.run)
     {
-
-
         loop(&CH);
-        printf("I:%03x PC:%04x SP:%04x ",CH.I, CH.pc,CH.sp);
-        for(int i = 0; i <= 0xf; i++)
-        {
-            printf("V%01x:%02x ",i, CH.V[i]);
-        }
-        if((CH.opcode&0xF0FF) == 0xf00A){
-            scanf("%x",&CH.V[CH.opcode&0x0F00>>8]);
-            CH.pc+=2;
-        }
-
-        getchar();
-
-
-
     }
     return 0;
 }
